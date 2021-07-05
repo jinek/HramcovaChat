d61492ff656eaae98050ea63c2bd4dccb7744b8b
Evgeny Gorbovoy <Evgeny.Gorbovoy@deltatre.com>
2021-07-05 21:23:06 +0200

    Chat console client added

---
+++ src/ChatConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
